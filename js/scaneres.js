f76a27e36e966ab32c4493515b35cfa24c95a080
leandro <>
2023-11-26 20:35:30 -0300

    carrito finalizado falta detalle
    listo pero no listo

---
+++ js/scaneres.js
@@ -0,0 +1,23 @@
+/* Esta lista podría ser reemplazada por la respuesta de un backend */
+const bicicletas = [
+    {
+      id:1,
+      nombre: "Escaner 1 ",
+      precio: 50000,
+    },
+    {
+      id:2,
+      nombre: "Escamer 2",
+      precio: 75000,
+    },
+    {
+      id:3,
+      nombre: "Escaner 3",
+      precio: 85000,
+    },
+    {
+      id:4,
+      nombre: "Escaner 4",
+      precio: 100000,
+    },
+  ]
